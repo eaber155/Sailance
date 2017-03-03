04250e7c5c12ebcb7773687f5ac5cee671090fe8
Eunice Patricia Aber <eaber155@gmail.com>
2017-03-03 14:23:42 +0300

    Sailance

---
+++ app/src/main/java/com/evivian/admin/sailance/MainActivity.java
@@ -0,0 +1,13 @@
+package com.evivian.admin.sailance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
